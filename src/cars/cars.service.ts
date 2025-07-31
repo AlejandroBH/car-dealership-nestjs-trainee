d712dfa286667ccef560c42a7de76801f04a8ed6
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-07-30 20:07:57 -0400

    seed inject dependecy fix

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -62,4 +62,8 @@ export class CarsService {
     this.cars = this.cars.filter((car) => car.id !== id);
     return car;
   }
+
+  public fillCarsWithSeedData(cars: Car[]) {
+    this.cars = cars;
+  }
 }
